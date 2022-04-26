c9d4cc51fad18299e5cc8d8752395bb06c502475
kamisora <1210281722@qq.com>
2022-04-26 12:17:19 +0800

    version0.0003

---
+++ src/_883.java
@@ -0,0 +1,21 @@
+public class _883 {
+    public int projectionArea(int[][] grid) {
+        //xy面积
+        int xy = 0, zx = 0, yz = 0, sum = 0;
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            int a = 0, b = 0;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    xy++;
+                }
+                a = Math.max(a, grid[i][j]);//左视图
+                b = Math.max(b, grid[j][i]);//主视图
+            }
+            yz += a;
+            zx += b;
+        }
+        sum = xy + zx + yz;
+        return sum;
+    }
+}
