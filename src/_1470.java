e762217ef45bef6216f67a5dad189e83db9ae595
kamisora <1210281722@qq.com>
2022-08-29 11:53:08 +0800

    version0.0025

---
+++ src/_1470.java
@@ -0,0 +1,13 @@
+public class _1470 {
+    /**
+     * 1470. 重新排列数组
+     */
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[i + n];
+        }
+        return ans;
+    }
+}
