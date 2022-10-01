b6f93ca1584875a96b71e2309a6c2ae54174e474
kamisora <1210281722@qq.com>
2022-10-01 11:32:59 +0800

    1.0

--- Algorithm/1/_122.cpp
+++ Algorithm/1/_122.cpp
@@ -33,4 +33,28 @@ public:
         }
         return noHold;
     }
+    //贪心
+    int maxProfit3(vector<int>& prices) {
+        //贪心
+        if(prices.empty() || prices.size()<2){
+            return 0;
+        }
+        int total = 0,index = 0,length = prices.size();
+        while(index <length){
+            //开始寻找股票上涨的点(如果找不到就一直找)
+            while(index<length-1 && prices[index]>= prices[index+1]){
+                index++;
+            }
+            //此时找到了上涨的最小点
+            int min = prices[index];
+            //找股票上涨的最大点直到为止
+            while(index < length-1 && prices[index] <= prices[index+1]){
+                index++;
+            }
+            //此时找到了股票上涨的最大点 index++
+            int max = prices[index++];
+            total += max-min;
+        }
+        return total;
+    }
 };
