c2e24953d60fee44eb783c099af6aad843809b27
kamisora <1210281722@qq.com>
2022-10-13 18:59:52 +0800

    2.0

---
+++ Algorithm/subjects/141. 环形链表/main.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    //1.快慢指针
+    public boolean hasCycle_1(ListNode head) {
+        //使用快慢指针，如果相遇就说明有环存在
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //2.使用集合判断
+    public boolean hasCycle_2(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
