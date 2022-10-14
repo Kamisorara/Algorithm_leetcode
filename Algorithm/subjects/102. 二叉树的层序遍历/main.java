2050599a88bf65dde0c062e6a4a2d1da9225d3e9
kamisora <1210281722@qq.com>
2022-10-14 14:00:15 +0800

    2.0

---
+++ Algorithm/subjects/102. 二叉树的层序遍历/main.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        //将根节点放入队列中，然后不断遍历队列
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            //获取当前队列的长度，这个长度相当于 当前这一层的节点个数
+            int queueCurrentSize = queue.size();
+            //将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
+            //如果节点的左/右子树不为空，也放入队列中
+            for (int i = 0; i < queueCurrentSize; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            //将临时list加入最终返回结果中
+            res.add(temp);
+        }
+        return res;
+    }
+}
