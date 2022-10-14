42debf025d2d3d4e462ec9356735e94cc7002e22
kamisora <1210281722@qq.com>
2022-10-14 13:30:07 +0800

    2.0

---
+++ Algorithm/subjects/98. 验证二叉搜索树/main2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        //迭代中序遍历
+        //因为在二叉搜索树中使用中中序遍历那么得到的值一定义升序的
+        Deque<TreeNode> stack = new LinkedList<>();
+        long inorder = Long.MIN_VALUE;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= inorder) {
+                return false;
+            }
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
