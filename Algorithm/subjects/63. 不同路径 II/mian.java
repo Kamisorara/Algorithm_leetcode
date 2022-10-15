e6083b98f0aebe0bca870ce60dd5dcb8dc91a529
kamisora <1210281722@qq.com>
2022-10-15 20:41:59 +0800

    2.0

---
+++ Algorithm/subjects/63. 不同路径 II/mian.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int rows = grid.length, cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+        dp[0][0] = 1;
+        if (grid[0][0] == 1) {
+            return 0;
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i > 0 && j > 0 && grid[i][j] != 1) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else if (i > 0 && grid[i][j] != 1) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (j > 0 && grid[i][j] != 1) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[rows - 1][cols - 1];
+    }
+}
