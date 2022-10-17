3f9d620b6ab67d3ad81305aed29321d1cd5af4d6
kamisora <1210281722@qq.com>
2022-10-17 22:59:38 +0800

    2.0

--- Algorithm/subjects/904. 水果成篮/main.java
+++ Algorithm/subjects/904. 水果成篮/main.java
@@ -1,23 +1,42 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
-        // 区间内两个连续子数的最大值
-        // 采用滑动窗口
-        int maxVal = 0, n = fruits.length;
-        int left = 0, right = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        while (right < n) {
-            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
-            while (map.size() > 2) {
-                //如果存在第三个数逐渐移动left 并移除fruit[left]
-                map.put(fruits[left], map.get(fruits[left]) - 1);
-                if (map.get(fruits[left]) == 0) {
-                    map.remove(fruits[left]);
-                }
-                left++;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        //最后一定要留出两个数的空位
+        for (int k = 0; k < nums.length - 2; k++) {
+            //因为这个数组升序了，如果nums[k] > 0 则说明没有复数了可以break掉
+            if (nums[k] > 0) {
+                break;
             }
-            maxVal = Math.max(maxVal, right - left + 1);
-            right++;
+            if (k > 0 && nums[k] == nums[k - 1]) {
+                continue;  // 给数组去重
+            }
+            //定义左右指针
+            int left = k + 1, right = nums.length - 1;
+            while (left < right) {
+                int num = nums[k] + nums[left] + nums[right];
+                //如果现在的 num 太大了 则说明 当前的 right 太大了
+                if (num > 0) {
+                    //移动right 并去重
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    right--;
+                    // 如果现在的num 太大了则说明当前的 left 太小了 
+                } else if (num < 0) {
+                    //移动left 并去重
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    left++;
+                    //此时正好等于 0 了
+                } else {
+                    //添加至res中
+                    res.add(new ArrayList<>(Arrays.asList(nums[k], nums[left], nums[right])));
+                    //移动left 和 right 并去重
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+            }
         }
-        return maxVal;
+        return res;
     }
 }
