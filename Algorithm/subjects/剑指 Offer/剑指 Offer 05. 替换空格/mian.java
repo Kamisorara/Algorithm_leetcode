bb62b6dda6bd1f9a6bff4d61bcb7fc630130c45e
kamisora <1210281722@qq.com>
2022-10-21 15:11:33 +0800

    2.0

--- Algorithm/subjects/剑指 Offer/剑指 Offer 05. 替换空格/mian.java
+++ Algorithm/subjects/剑指 Offer/剑指 Offer 05. 替换空格/mian.java
@@ -1,6 +1,21 @@
 class Solution {
+    // 直接调用 api的方法
     public String replaceSpace(String s) {
         String newStr = s.replace(" ", "%20");
         return newStr;
     }
+    //老老实实进行字符串拼接
+    public String replaceSpace(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != ' ') {
+                sb.append(chars[i]);
+            } else {
+                sb.append("%20");
+            }
+        }
+        return sb.toString();
+    }
+
 }
