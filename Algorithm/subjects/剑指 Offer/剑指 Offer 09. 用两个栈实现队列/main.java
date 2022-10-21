4de57f6c6844433923916acc08b13d32216ed003
kamisora <1210281722@qq.com>
2022-10-21 16:51:58 +0800

    2.0

---
+++ Algorithm/subjects/剑指 Offer/剑指 Offer 09. 用两个栈实现队列/main.java
@@ -0,0 +1,22 @@
+class CQueue {
+
+    LinkedList<Integer> queue1;
+
+    public CQueue() {
+        queue1 = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        queue1.addLast(value);
+    }
+
+    public int deleteHead() {
+        return queue1.size() == 0 ? -1 : queue1.removeFirst();
+    }
+}
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
