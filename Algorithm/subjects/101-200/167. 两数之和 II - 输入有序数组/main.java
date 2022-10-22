8b723bb849e4a2f4c610142850acdd0594b5a66c
kamisora <1210281722@qq.com>
2022-10-22 20:03:53 +0800

    2.0

--- Algorithm/subjects/101-200/167. 两数之和 II - 输入有序数组/main.java
+++ Algorithm/subjects/101-200/167. 两数之和 II - 输入有序数组/main.java
@@ -11,4 +11,22 @@ class Solution {
         }
         return new int[]{1, 2};
     }
+
+
+    // 双指针 时间复杂度 O(n) (正确的解法)
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int num = numbers[left] + numbers[right];
+            if (num < target) {
+                left++;
+            } else if (num > target) {
+                right--;
+            } else {
+                return new int[]{left + 1,right + 1};
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }
